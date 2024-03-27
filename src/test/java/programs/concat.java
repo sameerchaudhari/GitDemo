b8b2de28a93bc87464f9bd38aea1945d35dfcbfa
sameerchaudhari <sameerchaudhari088@gmail.com>
2024-03-27 14:21:05 +0530

    this is commit from asian time zone person

---
+++ src/test/java/programs/concat.java
@@ -0,0 +1,17 @@
+package programs;
+
+public class concat {
+	
+public static void main(String[] args) {
+	
+	String str1 = "Hello"; 
+	String str2 = "World";
+	
+	String str3;
+	
+	str3 = str1.concat(str2);
+	
+	System.out.println(str3);
+}
+
+}
